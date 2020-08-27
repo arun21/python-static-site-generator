365bc8f034c89a174ae6cb8df6fb0169a13b1acd
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-08-26 23:45:54 -0400

    Parser Subclasses

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -41,7 +41,7 @@ class MarkdownParser(Parser):
     def parse(self, path, source, dest):
         content = Content.load(self.read(path))
         html = markdown(content.body)
-        self.write(html,dest)
+        self.write(html,dest, content)
         sys.stdout.write("\x1b[1;32m{} converted to HTML. Metadata: {}\n".
                          format(path.name, content))
 
