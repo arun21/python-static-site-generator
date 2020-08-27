b429b9b42beb9dd99f45d49934f57e8b7b3f1397
arun.prabhu <arun.prabhu@mobilelive.ca>
2020-08-26 23:17:44 -0400

    The Content Class

--- ssg/content.py
+++ ssg/content.py
@@ -4,13 +4,14 @@ from collections.abc import Mapping
 
 
 class Content(Mapping):
-    __delimiter = "^(?:-|\+){3}\s*$"
+    __delimiter = r"^(?:-|\+){3}\s*$"
     __regex = re.compile(__delimiter, re.MULTILINE)
 
-    def load(self, cls, string):
-        _, fm, content = self.__regex.split(string, 2)
-        load(fm, Loader=FullLoader)
-        return cls(self.metadata, content)
+    @classmethod
+    def load(cls, string):
+        _, fm, content = cls.__regex.split(string, 2)
+        metadata = load(fm, Loader=FullLoader)
+        return cls(metadata, content)
 
     def __init__(self, metadata, content):
         self.data = metadata
@@ -22,4 +23,24 @@ class Content(Mapping):
 
     @property
     def type(self):
-        return self.data["type"] if self.data["type"] else None
+        return self.data["type"] if "type" in self.data else None
+
+    @type.setter
+    def type(self, type):
+        self.data["type"] = type
+
+    def __getitem__(self, key):
+        return self.data[key]
+
+    def __iter__(self):
+        self.data.__iter__()
+
+    def __len__(self):
+        return len(self.data)
+
+    def __repr(self):
+        data = {}
+        for k, v in self.data.items():
+            if k != "content":
+                data[k] = v
+        return str(data)
